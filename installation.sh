18fb15e16bcd51821fe788b0a7c2fea10f9a321c
Tal Arie <93095821+talari-bgu@users.noreply.github.com>
2024-09-22 17:08:46 +0300

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,3 +1,9 @@
-# add to ~/.bashrc
+# export to ~/.bashrc
+echo 'source ~/roverrobotics_ros2/install/setup.sh' >> ~/.bashrc
+echo 'export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp' >> ~/.bashrc
 
 # add etc/udev/rules.d/rov
+cd ~/roverrobotics_ros2
+sudo cp 55-roverrobotics.rules /etc/udev/rules.d/55-roverrobotics.rules
+sudo udevadm control --reload-rules
+sudo udevadm trigger
