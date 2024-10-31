34e424c88b2457de43df9706b0778326b6277e8c
Tal Arie <tuli19011901@gmail.com>
2024-10-31 12:29:30 +0200

    change param script

--- src/roverrobotics_driver/launch/navigation_launch.py
+++ src/roverrobotics_driver/launch/navigation_launch.py
@@ -126,7 +126,7 @@ def generate_launch_description():
     
     declare_slam_map_file_cmd = DeclareLaunchArgument(
         'map_file_name',
-        default_value='maze_map',
+        default_value='labs',
         description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
     
     map_file_arg = PathJoinSubstitution([
